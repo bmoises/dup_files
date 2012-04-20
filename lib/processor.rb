85e81aa98ac7f76b54a8cc576c308ad3c03c06d6
Moises Beltran <BMoises@gmail.com>
2012-04-19 23:40:50 -0700

    Optimized the indexing of files. 106M Test directory was processed in 770 ms vs 5500 ms.

--- lib/processor.rb
+++ lib/processor.rb
@@ -12,8 +12,39 @@ class Processor
     end
   end
   
+  CHUNK = 32
   def md5(path)
-    Digest::MD5.file(path)
+    # We will sample the file
+    file = File.new(path,"r")
+
+    size = file.size
+
+    str = ""
+    if size < (2 * CHUNK)
+      str << file.read
+    else
+      # read begining
+      str << file.read(CHUNK)
+
+      # read middle
+      if size > (5 * CHUNK)
+        file.seek( (size / 4) )
+        str << file.read(CHUNK)
+
+        file.seek( (size / 2) )
+        str << file.read(CHUNK)
+
+        file.seek( (size / 4) + (size / 2) )
+        str << file.read(CHUNK)
+      end
+
+      # read end
+      file.seek(size-CHUNK)
+      str << file.read(CHUNK)
+    end
+    res = Digest::MD5.hexdigest(str)
+    file.close
+    res
   end
   
   module FileTable
@@ -24,7 +55,7 @@ class Processor
   
   def put_in_db(path,digest)
     vals = [path , 
-            digest, 
+            digest,
             File.ctime(path), 
             File.mtime(path)].join("','")
     
